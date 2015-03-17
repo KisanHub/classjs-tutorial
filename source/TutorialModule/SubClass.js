69719c460925cfa411f07a5eb16f2828fdaad597
Raphael Cohn <raphael.cohn@stormmq.com>
2015-03-17 12:39:34 +0000

    With copyright notices

--- source/TutorialModule/SubClass.js
+++ source/TutorialModule/SubClass.js
@@ -1,3 +1,9 @@
+/*
+This file is part of classjs-tutorial. It is subject to the licence terms in the COPYRIGHT file found in the top-level directory of this distribution and at https://raw.githubusercontent.com/KisanHub/classjs-tutorial/master/COPYRIGHT. No part of classjs-tutorial, including this file, may be copied, modified, propagated, or distributed except according to the terms contained in the COPYRIGHT file.
+Copyright © 2015 The developers of classjs-tutorial. See the COPYRIGHT file in the top-level directory of this distribution and at https://raw.githubusercontent.com/KisanHub/classjs-tutorial/master/COPYRIGHT.
+*/
+
+
 module.BaseClass.extend
 (
 	module,
